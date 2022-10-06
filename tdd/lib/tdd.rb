a1cd66431db0eb75b56c818a92716e027aebfcbb
Justin Kilburn <ThatMoonMan@gmail.com>
2022-10-06 14:21:05 -0400

    #two_sum specs

--- tdd/lib/tdd.rb
+++ tdd/lib/tdd.rb
@@ -7,4 +7,13 @@ class Array
         new_arr
     end
 
+    def two_sum
+        new_arr = []
+            self.each_with_index do |ele1, i1|
+                self.each_with_index do |ele2, i2|
+                    new_arr << [i1, i2] if i2 > i1 && ele1 + ele2 == 0
+                end
+            end
+        new_arr
+    end
 end
--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -3,7 +3,7 @@ require "rspec"
 
 
 describe "#my_uniq" do
-    array = [1, 2, 3, 3]
+    subject(:array)  { [1, 2, 3, 3] }
     it "checks that the method receives an array" do
         expect(array).to respond_to(:my_uniq)
         array.my_uniq
@@ -17,4 +17,19 @@ describe "#my_uniq" do
     it "should return an array of unique values" do
         expect(array.my_uniq).to eq([1,2,3])
     end
+end
+
+describe "#two_sum" do
+    subject(:array)  { [1, 2, -3, 3] }
+    it "checks that the method receives an array" do
+        expect(array).to respond_to(:two_sum)
+        array.two_sum
+    end
+    it "returns a new array" do
+        expect(array.two_sum).not_to be(array)
+    end
+    it "returns indices of unique elements that sum to zero" do
+        expect(array.two_sum).to eq([[2,3]])
+    end
+
 end
